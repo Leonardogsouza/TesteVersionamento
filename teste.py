65f73ba113446fc5eb4bbcdb1758c6c5c19e3f4c
Leonardo Gonçalves de Souza <leonardosouza291129@gmail.com>
2026-08-20 20:33:04 -0300

    Adiciona arquivos teste.py e testeprojeto.html

---
+++ teste.py
@@ -0,0 +1,15 @@
+nome = input("digite o seu nome")
+idade = int(input("digite sua idade: "))
+
+
+if idade >= 16:
+        print("pode votar")
+
+        
+
+
+
+
+
+print(f"o nome do usuario é {nome}")
+print(f"sua idade é: {idade}")
